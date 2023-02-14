d8d3fffaded3107208a2306b45b57ab150b78b0f
ggongjukim <kch7892003@gmail.com>
2023-02-14 21:30:48 +0900

    ✨ feat: add input 컴포넌트

--- src/todos/index.tsx
+++ src/todos/index.tsx
@@ -1,19 +1,23 @@
 // import AddTodoButton from './components/addtodobutton';
 import { ReactComponent as AddIcon } from './assets/addIcon.svg';
 import ListItem from './components/listitem';
+import * as S from './styled';
 import Input from '../commons/components/input';
 
 const Todos = () => (
   <>
-    <Input />
-    <AddIcon />
-    <ul style={{ listStyle: 'none' }}>
+    <S.AddContainer>
+      <Input name="할일 추가" />
+      <button>
+        <AddIcon />
+      </button>
+    </S.AddContainer>
+
+    <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
       <li>
-        {' '}
         <ListItem />
       </li>
       <li>
-        {' '}
         <ListItem />
       </li>
     </ul>
---
+++ src/todos/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AddContainer = styled.div`
+  width: 920px;
+  margin: 50px auto;
+
+  display: flex;
+  /* border: 1px solid pink; */
+  flex-direction: row;
+
+  button {
+    padding-left: 20px;
+    background-color: white;
+  }
+`;
